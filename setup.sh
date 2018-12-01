830b624672ce47bbadc4ea901ee8a42c9463431f
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2018-11-30 23:55:49 -0500

    fixes and enhancements to setup.sh

--- setup.sh
+++ setup.sh
@@ -20,28 +20,44 @@ sudo -H pip ${PIP_FLAG} install unidecode
 sudo -H pip ${PIP_FLAG} install peewee
 sudo -H pip ${PIP_FLAG} install beautifulsoup4
 
+REP="t"
+
 RUN_LOGIN_INFO=""
 
 echo "Do you wish to create or update MySQL login information? (y/n)"
 
-read RUN_LOGIN_INFO
+while [ ${REP} ]; do
+  read RUN_LOGIN_INFO
 
-if [ ${RUN_LOGIN_INFO} = "y" -o "yes" ]; then
-  echo "Running login_info.sh..."
-  sh bin/login_info.sh
-else
-  echo "Not generating MySQL login file"
+  case "$RUN_LOGIN_INFO" in
+    "yes"|"y")  echo "Running login_info.sh..."; sh bin/login_info.sh; REP="";;
+    "no"|"n"|"c")  echo "Not generating MySQL login file"; REP=""; RUN_LOGIN_INFO="";;
+    *) echo "invalid option: ${RUN_LOGIN_INFO}. Please answer with y or n.";;
+  esac
+done
+
+REP="t"
+
+echo "Do you wish to create the required tables in your database? (y/n) NOTE: Will not work without first creating MySQL login information file."
+
+while [ ${REP} ]; do
+  read CREATE_TABLES
+
+  case "$CREATE_TABLES" in
+    "yes"|"y")  echo "Running make_tables.py..."; python lib/make_tables.py; REP="";;
+    "no"|"n")  echo "Not creating tables"; REP=""; CREATE_TABLES="";;
+    *) echo "invalid option: ${RUN_LOGIN_INFO}. Please answer with y or n.";;
+  esac
+done
+
+echo "\n"
+echo "Setup complete!"
+echo "Verified installation of:\nunidecode\npeewee\nbeautifulsoup4"
+if [ ${RUN_LOGIN_INFO} ]; then
+  echo "Updated lib/mysql_login_info.py"
 fi
 
-echo "Do you wish to create the required tables in your database?"
-
-read CREATE_TABLES
-
-if [ ${CREATE_TABLES} = "y" -o "yes" ]; then
-  echo "Running make_tables.py..."
-  python lib/make_tables.py
-else
-  echo "Not creating tables"
+if [ ${CREATE_TABLES} ]; then
+  echo "Created tables in MySQL database"
 fi
-
 exit 0
